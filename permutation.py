d9c739137d2e5969a8304ed99dac0d5b02e1dc97
kyoongdev <9898junjun2@gmail.com>
2024-02-21 19:47:35 +0900

    feat: permutation by python

---
+++ permutation.py
@@ -0,0 +1,15 @@
+def permutation(items, maxLength, result, output):
+  if len(result) == maxLength:
+    output.append(result)
+    result = []
+    return
+  
+  for item in items:
+    copiedResult = result.copy()
+    copiedResult.append(item)
+    permutation(items, maxLength, copiedResult, output)
+
+output = []
+permutation([1,2,3,4], 3, [],output)
+
+print(output)
