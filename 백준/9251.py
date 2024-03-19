e2f5f25b3da16512febb93b61c96d6048ee7a814
kyoongdev <9898junjun2@gmail.com>
2024-03-19 16:41:22 +0900

    feat: 9251

---
+++ 백준/9251.py
@@ -0,0 +1,13 @@
+lineOne = input()
+lineTwo = input()
+
+dp = [[0] * (len(lineTwo) + 1) for _ in range(len(lineOne) + 1)]
+
+for i in range(1, len(lineOne) + 1):
+    for j in range(1, len(lineTwo) + 1):
+        if lineOne[i - 1] == lineTwo[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[-1][-1])
