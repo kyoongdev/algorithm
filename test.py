7b8ac4549c738ab08e824a8665a7ba645552406f
kyoongdev <9898junjun2@gmail.com>
2024-03-22 17:44:50 +0900

    feat: 9084

--- test.py
+++ test.py
@@ -1,33 +1,15 @@
-from collections import deque
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**9)
+N,K = list(map(int,input().split()))
 
-R, C = list(map(int,input().split()))
+coins = []
+for _ in range(N):
+  coins.append(int(input()))
 
+dp = [0] * (K + 1)
 
-boards = []
-for _ in range(R):
-  boards.append([x for x in input()])
+dp[0] = 1
 
+for coin in coins:
+  for i in range(coin, K + 1):
+    dp[i] = dp[i - coin] + dp[i]
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-maxCount = 0
-
-def dfs(start,move):
-  global maxCount 
-  x,y = start
-  # print(x,y,move)
-  maxCount = max(maxCount,len(move))
-
-  for i in range(4):
-    nx,ny = x + dx[i], y + dy[i]
-
-    if 0 <= nx < R and 0 <= ny < C and boards[nx][ny] not in move:
-      copiedMoved = move.copy()
-      copiedMoved.add(boards[nx][ny])
-      dfs((nx,ny),copiedMoved)
-
-dfs((0,0),{boards[0][0]})
-print(maxCount)
+print(dp[K])
---
+++ 백준/9084.py
@@ -0,0 +1,14 @@
+T = int(input())
+
+for _ in range(T):
+  N = int(input())
+
+  coins = list(map(int,input().split()))
+  M = int(input())
+  dp = [0] * (M + 1)
+  dp[0] = 1
+  for coin in coins:
+    for i in range(coin,M + 1):
+      dp[i] += dp[i - coin]
+
+  print(dp[M])
